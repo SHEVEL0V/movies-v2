ba1de36875088632340f142c75be7f33c4456d61
Андрій Шевельов <ashevelevmti@gmail.com>
2022-09-04 17:14:39 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { lazy, Suspense } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Header from './components/header';
-import Container from './components/container';
-import Loading from './components/loading';
-import Error from './components/error';
+/** @format */
+
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Header from "./components/header";
+import Container from "./components/container";
+import Loading from "./components/loading";
+import Error from "./components/error";
 
 const queryClient = new QueryClient();
-const HomePage = lazy(() => import('./views/home'));
-const MovieDetailsPage = lazy(() => import('./views/details'));
-const SerchFilm = lazy(() => import('./views/search'));
+const HomePage = lazy(() => import("./views/home"));
+const MovieDetailsPage = lazy(() => import("./views/details"));
+const SerchFilm = lazy(() => import("./views/search"));
 
 export default function App() {
   return (
